5f7bd2a0b5cf0601e4e8f03a026b7bc1d93857fd
Sergio Chouhy <sergio.chouhy@gmail.com>
2021-04-04 19:13:02 +0000

    replace board

--- chess/__init__.py
+++ chess/__init__.py
@@ -126,15 +126,13 @@ STATUS_IMPOSSIBLE_CHECK = Status.IMPOSSIBLE_CHECK
 
 Square = int
 SQUARES = [
-    A1, B1, C1, D1, E1, F1, G1, H1,
-    A2, B2, C2, D2, E2, F2, G2, H2,
-    A3, B3, C3, D3, E3, F3, G3, H3,
-    A4, B4, C4, D4, E4, F4, G4, H4,
-    A5, B5, C5, D5, E5, F5, G5, H5,
-    A6, B6, C6, D6, E6, F6, G6, H6,
-    A7, B7, C7, D7, E7, F7, G7, H7,
-    A8, B8, C8, D8, E8, F8, G8, H8,
-] = range(64)
+    A1, B1, C1, D1, E1,
+    A2, B2, C2, D2, E2,
+    A3, B3, C3, D3, E3,
+    A4, B4, C4, D4, E4,
+    A5, B5, C5, D5, E5,
+    A6, B6, C6, D6, E6,
+] = range(30)
 
 SQUARE_NAMES = [f + r for r in RANK_NAMES for f in FILE_NAMES]
 
@@ -178,7 +176,7 @@ SQUARES_180 = [square_mirror(sq) for sq in SQUARES]
 
 Bitboard = int
 BB_EMPTY = 0
-BB_ALL = 0xffff_ffff_ffff_ffff
+BB_ALL = 0x3fffffff
 
 BB_SQUARES = [
     BB_A1, BB_B1, BB_C1, BB_D1, BB_E1, BB_F1, BB_G1, BB_H1,
@@ -192,21 +190,18 @@ BB_SQUARES = [
 ] = [1 << sq for sq in SQUARES]
 
 BB_CORNERS = BB_A1 | BB_H1 | BB_A8 | BB_H8
-BB_CENTER = BB_D4 | BB_E4 | BB_D5 | BB_E5
+# BB_CENTER = BB_D4 | BB_E4 | BB_D5 | BB_E5
 
-BB_LIGHT_SQUARES = 0x55aa_55aa_55aa_55aa
-BB_DARK_SQUARES = 0xaa55_aa55_aa55_aa55
+BB_LIGHT_SQUARES = 0x14a5294a 
+BB_DARK_SQUARES = 0x2b5ad6b5 
 
 BB_FILES = [
     BB_FILE_A,
     BB_FILE_B,
     BB_FILE_C,
     BB_FILE_D,
-    BB_FILE_E,
-    BB_FILE_F,
-    BB_FILE_G,
-    BB_FILE_H,
-] = [0x0101_0101_0101_0101 << i for i in range(8)]
+    BB_FILE_E
+] = [0x0101_0101_0101_0101 << i for i in range(5)]
 
 BB_RANKS = [
     BB_RANK_1,
@@ -215,9 +210,7 @@ BB_RANKS = [
     BB_RANK_4,
     BB_RANK_5,
     BB_RANK_6,
-    BB_RANK_7,
-    BB_RANK_8,
-] = [0xff << (8 * i) for i in range(8)]
+] = [0x1f << (5 * i) for i in range(6)]
 
 BB_BACKRANKS = BB_RANK_1 | BB_RANK_8
 
@@ -247,39 +240,39 @@ except AttributeError:
     def popcount(bb: Bitboard) -> int:
         return bin(bb).count("1")
 
-def flip_vertical(bb: Bitboard) -> Bitboard:
-    # https://www.chessprogramming.org/Flipping_Mirroring_and_Rotating#FlipVertically
-    bb = ((bb >> 8) & 0x00ff_00ff_00ff_00ff) | ((bb & 0x00ff_00ff_00ff_00ff) << 8)
-    bb = ((bb >> 16) & 0x0000_ffff_0000_ffff) | ((bb & 0x0000_ffff_0000_ffff) << 16)
-    bb = (bb >> 32) | ((bb & 0x0000_0000_ffff_ffff) << 32)
-    return bb
-
-def flip_horizontal(bb: Bitboard) -> Bitboard:
-    # https://www.chessprogramming.org/Flipping_Mirroring_and_Rotating#MirrorHorizontally
-    bb = ((bb >> 1) & 0x5555_5555_5555_5555) | ((bb & 0x5555_5555_5555_5555) << 1)
-    bb = ((bb >> 2) & 0x3333_3333_3333_3333) | ((bb & 0x3333_3333_3333_3333) << 2)
-    bb = ((bb >> 4) & 0x0f0f_0f0f_0f0f_0f0f) | ((bb & 0x0f0f_0f0f_0f0f_0f0f) << 4)
-    return bb
-
-def flip_diagonal(bb: Bitboard) -> Bitboard:
-    # https://www.chessprogramming.org/Flipping_Mirroring_and_Rotating#FlipabouttheDiagonal
-    t = (bb ^ (bb << 28)) & 0x0f0f_0f0f_0000_0000
-    bb = bb ^ (t ^ (t >> 28))
-    t = (bb ^ (bb << 14)) & 0x3333_0000_3333_0000
-    bb = bb ^ (t ^ (t >> 14))
-    t = (bb ^ (bb << 7)) & 0x5500_5500_5500_5500
-    bb = bb ^ (t ^ (t >> 7))
-    return bb
-
-def flip_anti_diagonal(bb: Bitboard) -> Bitboard:
-    # https://www.chessprogramming.org/Flipping_Mirroring_and_Rotating#FlipabouttheAntidiagonal
-    t = bb ^ (bb << 36)
-    bb = bb ^ ((t ^ (bb >> 36)) & 0xf0f0_f0f0_0f0f_0f0f)
-    t = (bb ^ (bb << 18)) & 0xcccc_0000_cccc_0000
-    bb = bb ^ (t ^ (t >> 18))
-    t = (bb ^ (bb << 9)) & 0xaa00_aa00_aa00_aa00
-    bb = bb ^ (t ^ (t >> 9))
-    return bb
+# def flip_vertical(bb: Bitboard) -> Bitboard:
+#     # https://www.chessprogramming.org/Flipping_Mirroring_and_Rotating#FlipVertically
+#     bb = ((bb >> 8) & 0x00ff_00ff_00ff_00ff) | ((bb & 0x00ff_00ff_00ff_00ff) << 8)
+#     bb = ((bb >> 16) & 0x0000_ffff_0000_ffff) | ((bb & 0x0000_ffff_0000_ffff) << 16)
+#     bb = (bb >> 32) | ((bb & 0x0000_0000_ffff_ffff) << 32)
+#     return bb
+# 
+# def flip_horizontal(bb: Bitboard) -> Bitboard:
+#     # https://www.chessprogramming.org/Flipping_Mirroring_and_Rotating#MirrorHorizontally
+#     bb = ((bb >> 1) & 0x5555_5555_5555_5555) | ((bb & 0x5555_5555_5555_5555) << 1)
+#     bb = ((bb >> 2) & 0x3333_3333_3333_3333) | ((bb & 0x3333_3333_3333_3333) << 2)
+#     bb = ((bb >> 4) & 0x0f0f_0f0f_0f0f_0f0f) | ((bb & 0x0f0f_0f0f_0f0f_0f0f) << 4)
+#     return bb
+# 
+# def flip_diagonal(bb: Bitboard) -> Bitboard:
+#     # https://www.chessprogramming.org/Flipping_Mirroring_and_Rotating#FlipabouttheDiagonal
+#     t = (bb ^ (bb << 28)) & 0x0f0f_0f0f_0000_0000
+#     bb = bb ^ (t ^ (t >> 28))
+#     t = (bb ^ (bb << 14)) & 0x3333_0000_3333_0000
+#     bb = bb ^ (t ^ (t >> 14))
+#     t = (bb ^ (bb << 7)) & 0x5500_5500_5500_5500
+#     bb = bb ^ (t ^ (t >> 7))
+#     return bb
+# 
+# def flip_anti_diagonal(bb: Bitboard) -> Bitboard:
+#     # https://www.chessprogramming.org/Flipping_Mirroring_and_Rotating#FlipabouttheAntidiagonal
+#     t = bb ^ (bb << 36)
+#     bb = bb ^ ((t ^ (bb >> 36)) & 0xf0f0_f0f0_0f0f_0f0f)
+#     t = (bb ^ (bb << 18)) & 0xcccc_0000_cccc_0000
+#     bb = bb ^ (t ^ (t >> 18))
+#     t = (bb ^ (bb << 9)) & 0xaa00_aa00_aa00_aa00
+#     bb = bb ^ (t ^ (t >> 9))
+#     return bb
 
 
 def shift_down(b: Bitboard) -> Bitboard:
